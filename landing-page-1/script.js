d68d05ffa870056e72ae4399fae37bc4e436cf1c
Arun <arunkarthick0810@gmail.com>
2023-10-19 16:39:29 +0530

    simple landing-page

---
+++ landing-page-1/script.js
@@ -0,0 +1,7 @@
+function imageSlide(img) {
+  document.querySelector(".starbucks").src = `./assets/${img}`;
+}
+
+function changeColor(color) {
+  document.querySelector(".circle").style.background = color;
+}
